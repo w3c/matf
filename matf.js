e001204b1405a9141896b6dbf81d94ab64d6d7a5
Jan Jaap de Groot <info@janjaap.com>
2024-11-28 10:54:48 +0100

    Open generated file in browser automatically

--- matf.js
+++ matf.js
@@ -3,6 +3,7 @@ import path from 'path';
 import semver from 'semver';
 import markdownit from 'markdown-it';
 import ejs from 'ejs';
+import open from 'open';
 import { WcagPlugin } from './wcag.js';
 
 const root = process.cwd();
@@ -30,42 +31,42 @@ const readFiles = async (folder, extension) => {
 // Execute: init plugins, read files, render HTML
 const execute = async () => {
   console.log(`Initializing WCAG plugins...`);
-    const wcagPlugin = await WcagPlugin.init('wcag', 'https://www.w3.org/TR/WCAG22/');
-    const wcag2ictPlugin = await WcagPlugin.init('wcag2ict', 'https://www.w3.org/TR/wcag2ict-22/');
+  const wcagPlugin = await WcagPlugin.init('wcag', 'https://www.w3.org/TR/WCAG22/');
+  const wcag2ictPlugin = await WcagPlugin.init('wcag2ict', 'https://www.w3.org/TR/wcag2ict-22/');
 
-    const md = markdownit({ 
-      html: true 
-    })
-    .use(wcagPlugin)
-    .use(wcag2ictPlugin);
+  console.log(`Initializing markdownit...`);
+  const md = markdownit({ 
+    html: true 
+  })
+  .use(wcagPlugin)
+  .use(wcag2ictPlugin);
 
-    console.log(`Reading markdown files...`);
-    const map = await readFiles('wcag', '.md');
+  console.log(`Reading markdown files...`);
+  const map = await readFiles('wcag', '.md');
+  const keys = Array.from(map.keys()).sort(semver.compare);
 
-    const keys = Array.from(map.keys()).sort(semver.compare);
-    console.log('Sorted keys:', keys);
+  console.log(`Rendering HTML for ${keys.length} files...`)
+  const files = [];
+  for (const key of keys) {
+    const html = md.render(map.get(key));
+    files.push({
+      key,
+      html,
+    });
+  }
+  console.log(`Successfully rendered ${files.length} files to HTML...`);
 
-    const files = [];
-    for (const key of keys) {
-      const html = md.render(map.get(key));
-      files.push({
-        key,
-        html,
-      });
-    }
-    console.log(`Successfully rendered ${files.length} files to HTML`);
+  const templateFile = path.join(root, 'index.ejs');
+  console.log(`Rendering template ${templateFile}...`);
+  const template = await fs.readFile(templateFile, 'utf8');
+  const html = ejs.render(template, { files });
 
-    const templateFile = path.join(root, 'index.ejs');
-    console.log(`Reading template from ${templateFile}`);
-    const template = await fs.readFile(templateFile, 'utf8');
+  const indexFile = path.join(root, 'index.html');
+  console.log(`Writing output to ${indexFile}`);
+  await fs.writeFile(indexFile, html, 'utf8');
 
-    console.log(`Rendering template...`);
-    const html = ejs.render(template, { files });
-
-    const indexFile = path.join(root, 'index.html');
-    console.log(`Writing output to ${indexFile}`);
-    await fs.writeFile(indexFile, html, 'utf8');
-    console.log(`Finished writing output!`);
+  console.log(`Opening ${indexFile} in browser...`)
+  await open(indexFile, { wait: false });
 };
 
 // Run the script and ensure completion
@@ -75,6 +76,6 @@ execute()
     process.exit(0);
   })
   .catch((error) => {
-    console.error(`Error: ${error}`);
+    console.error(`Script encountered error: ${error}`);
     process.exit(1);
   });
